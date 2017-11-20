a8c2e0917d1257e85e41b9a1e96e44407a4197df
David Recuenco <david.recuenco@adidas-group.com>
2017-11-20 12:04:37 +0100

    FIX publish to gh-pages branch

--- publish.sh
+++ publish.sh
@@ -14,5 +14,6 @@ git commit -m "PUBLISH v${PKG_VERSION}"
 git tag -f -a $PKG_VERSION -m $PKG_VERSION
 git remote add origin-publish https://${GITHUB_TOKEN}@github.com/RecuencoJones/test-travis-gh-pages-npm.git
 git push -f origin-publish develop:master
+git push -f origin-publish develop:gh-pages
 git push -f origin-publish --tags
 
