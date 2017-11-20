d05a888d2a761189de65aecf4361b82ac02c7cc7
David Recuenco <david.recuenco@adidas-group.com>
2017-11-20 11:05:13 +0100

    ADD github token

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
+
+PKG_VERSION=`node -p "require('./package.json').version"`
+npm run build:gh-pages
+git add -f ./dist
+git add -f ./docs
+git commit -m "PUBLISH v${PKG_VERSION}"
+git tag -a $PKG_VERSION -m $PKG_VERSION
+git remote add origin-publish https://${GITHUB_TOKEN}@github.com/RecuencoJones/test-travis-gh-pages-npm.git
+git push -f origin-publish develop:master
+
