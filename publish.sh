20ca4baa17d92d1f677c60ea6acdb4cffeda92e0
David Recuenco <david.recuenco@adidas-group.com>
2017-11-20 11:13:19 +0100

    FIX publish script

--- publish.sh
+++ publish.sh
@@ -10,5 +10,6 @@ git add -f ./docs
 git commit -m "PUBLISH v${PKG_VERSION}"
 git tag -a $PKG_VERSION -m $PKG_VERSION
 git remote add origin-publish https://${GITHUB_TOKEN}@github.com/RecuencoJones/test-travis-gh-pages-npm.git
-git push -f origin-publish develop:master
+git push -f origin-publish master
+git push origin-publish --tags
 
